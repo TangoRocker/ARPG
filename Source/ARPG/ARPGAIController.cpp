5fb647c5d88d73fa2d2f6d614e951cf95c2a856d
Dukler <iarwain@hotmail.com.ar>
2018-05-06 17:36:16 -0300

    Design corrections

--- Source/ARPG/ARPGAIController.cpp
+++ Source/ARPG/ARPGAIController.cpp
@@ -10,3 +10,15 @@ AARPGAIController::AARPGAIController()
 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	//PrimaryActorTick.bCanEverTick = true;
 }
+
+/*void AARPGAIController::BeginPlay()
+{
+
+
+}*/
+
+void AARPGAIController::FollowPlayer_Implementation()
+{
+
+
+}
--- Source/ARPG/ARPGAIController.h
+++ Source/ARPG/ARPGAIController.h
@@ -13,8 +13,17 @@ UCLASS()
 class ARPG_API AARPGAIController : public AAIController
 {
 	GENERATED_BODY()
-	
+
+public:
+	// Sets default values for this actor's properties
 	AARPGAIController();
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
+	void FollowPlayer();
+
+protected:
+	// Called when the game starts or when spawned
+	//UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
+	//virtual void BeginPlay() override;
 	
-	
+
 };
